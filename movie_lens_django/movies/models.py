4ec429f2e71437aea4db5cd8f0150d7f8a48584e
Gentil Ályson Jácome Fernandes <fernandes.gentilalysonjac@gmail.com>
2024-07-26 15:53:20 -0300

    feature(movies): adds Movie and MovieGenomeTag

--- movie_lens_django/movies/models.py
+++ movie_lens_django/movies/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from movie_lens_django.constants import LONG_SIZED_CHAR_FIELD
 from movie_lens_django.constants import MEDIUM_SIZED_CHAR_FIELD
+from movie_lens_django.genome.models import GenomeTag
 
 
 class Genre(models.Model):
@@ -13,3 +15,43 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=LONG_SIZED_CHAR_FIELD, verbose_name=_("Title"))
+    genres = models.ManyToManyField(to=Genre, verbose_name=_("Genres"))
+    genome_tags = models.ManyToManyField(to=GenomeTag, through="MovieGenomeTag")
+
+    class Meta:
+        verbose_name = _("Movie")
+        verbose_name_plural = _("Movies")
+
+    def __str__(self):
+        return self.title
+
+
+class MovieGenomeTag(models.Model):
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="movie_genome_tag",
+        verbose_name=_("Movie"),
+    )
+    genome_tag = models.ForeignKey(
+        GenomeTag,
+        on_delete=models.CASCADE,
+        related_name="movie_genome_tag",
+        verbose_name=_("Genome Tag"),
+    )
+    user_id = models.PositiveIntegerField(verbose_name=_("User ID"), unique=True)
+
+    class Meta:
+        verbose_name = _("Movie Genome Tags By User")
+        verbose_name_plural = _("Movie Genome Tags By User")
+
+    def __str__(self):
+        return (
+            f"Genome Tag: {self.genome_tag.tag} - "
+            f"Movie: {self.movie.title} - "
+            f"User: {self.user_id}"
+        )
