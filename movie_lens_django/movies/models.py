145b3b15052c9bf71a5b7147e05bed70cac6bffe
Gentil Ályson Jácome Fernandes <fernandes.gentilalysonjac@gmail.com>
2024-07-26 15:38:53 -0300

    feature(movies): adds Genre model

--- movie_lens_django/movies/models.py
+++ movie_lens_django/movies/models.py
@@ -1,1 +1,15 @@
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from movie_lens_django.constants import MEDIUM_SIZED_CHAR_FIELD
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=MEDIUM_SIZED_CHAR_FIELD, verbose_name=_("Name"))
+
+    class Meta:
+        verbose_name = _("Genre")
+        verbose_name_plural = _("Genres")
+
+    def __str__(self):
+        return self.name
