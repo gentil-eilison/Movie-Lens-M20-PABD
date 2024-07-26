0eea5658f801480a50d2455ffaa4e01170519718
Gentil Ályson Jácome Fernandes <fernandes.gentilalysonjac@gmail.com>
2024-07-26 15:36:45 -0300

    feature(genome): adds GenomeTag model

--- movie_lens_django/genome/models.py
+++ movie_lens_django/genome/models.py
@@ -1,1 +1,15 @@
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from movie_lens_django.constants import MEDIUM_SIZED_CHAR_FIELD
+
+
+class GenomeTag(models.Model):
+    tag = models.CharField(max_length=MEDIUM_SIZED_CHAR_FIELD, verbose_name=_("Tag"))
+
+    class Meta:
+        verbose_name = _("Genome Tag")
+        verbose_name_plural = _("Genome Tags")
+
+    def __str__(self):
+        return self.tag
