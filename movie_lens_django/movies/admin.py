e7307898fd1e6cbf550dae2425757c23a1bbf2ae
Gentil Ályson Jácome Fernandes <fernandes.gentilalysonjac@gmail.com>
2024-08-03 12:45:22 -0300

    feature: adds Genre creation

--- movie_lens_django/movies/admin.py
+++ movie_lens_django/movies/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from .models import Genre
 from .models import Movie
 
 admin.site.register(Movie)
+admin.site.register(Genre)
--- movie_lens_django/movies/concurrent_import.py
+++ movie_lens_django/movies/concurrent_import.py
@@ -5,12 +5,13 @@ from celery import shared_task
 
 from movie_lens_django.core.concurrent_import import ConcurrentImport
 from movie_lens_django.movies.forms import MovieForm
+from movie_lens_django.movies.models import Genre
 from movie_lens_django.movies.models import Movie
 
 
 class MoviesConcurrentImport(ConcurrentImport):
     @staticmethod
-    def __get_title_release_year(title_row: str):
+    def __get_title_release_year(title_row: str) -> tuple[str, str]:
         title = re.search(r".+(?=(\(\d{4}\)))", title_row)
         release_year = re.search(r"\(\d{4}\)", title_row)
         if title and release_year:
@@ -20,6 +21,16 @@ class MoviesConcurrentImport(ConcurrentImport):
             )
             return title, release_year
         return "", ""
+
+    @staticmethod
+    def __create_genres(genres_row: str) -> None:
+        row_genres_names = genres_row.split("|")
+        cleaned_genres_names = [genre_name.strip() for genre_name in row_genres_names]
+        for genre_name in cleaned_genres_names:
+            Genre.objects.get_or_create(
+                name=genre_name,
+                defaults={"name": genre_name},
+            )
 
     @staticmethod
     @shared_task
@@ -32,6 +43,7 @@ class MoviesConcurrentImport(ConcurrentImport):
             title, release_year = MoviesConcurrentImport.__get_title_release_year(
                 row["title"],
             )
+            MoviesConcurrentImport.__create_genres(row["genres"])
 
             if title and release_year:
                 form = MovieForm(data={"title": title, "release_year": release_year})
