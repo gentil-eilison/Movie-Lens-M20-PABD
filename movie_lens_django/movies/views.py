97cb64ef465abf984970862d8fdc0354f4e39018
Lucas <lucasoliveirars22@gmail.com>
2024-08-04 11:22:49 -0300

    feature(tags): base code import csv

--- movie_lens_django/movies/views.py
+++ movie_lens_django/movies/views.py
@@ -1,7 +1,15 @@
 from movie_lens_django.core.views import ConcurrentImportView
 from movie_lens_django.movies.concurrent_import import MoviesConcurrentImport
+from movie_lens_django.movies.concurrent_import_movie_tag import (
+    MovieTagConcurrentImport,
+)
 
 
 class ImportCSVMovieView(ConcurrentImportView):
     concurrent_import_class = MoviesConcurrentImport
     template_name = "movies/movie_import_form.html"
+
+
+class ImportCSVMovieTagView(ConcurrentImportView):
+    concurrent_import_class = MovieTagConcurrentImport
+    template_name = "movies/movie_tag_import_form.html"
