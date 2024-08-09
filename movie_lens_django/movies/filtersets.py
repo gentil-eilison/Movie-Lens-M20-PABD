0b2a5f423bf0dcaae639aa60cd27404e7f0b3257
Lucas <lucasoliveirars22@gmail.com>
2024-08-09 15:30:18 -0300

    feature(filters): adds basic movies template

--- movie_lens_django/movies/filtersets.py
+++ movie_lens_django/movies/filtersets.py
@@ -5,7 +5,7 @@ from movie_lens_django.movies.models import Movie
 
 class MovieFilterSet(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name="title", lookup_expr="icontains")
-    release_year = django_filters.NumericRangeFilter(field_name="release_year")
+    release_year = django_filters.RangeFilter(field_name="release_year")
 
     class Meta:
         model = Movie
